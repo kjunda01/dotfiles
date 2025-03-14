846ed91e46148414130eea24a840d14bd9f5730c
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-03-13 23:56:12 -0300

    Update: Atualização de scripts

--- scripts/.config/scripts/keyboard_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Aguarda 2 segundos antes de prosseguir
-sleep 2
-
-# Forçar o ambiente do X
-#[ -z "$DISPLAY" ] && export DISPLAY=:0
-setxkbmap -layout us -variant intl -option
---
+++ scripts/.config/scripts/samba_generate_smb.sh
@@ -0,0 +1,4 @@
+#
+echo "Copiando arquivo smb.conf para /etc/samba/smb.conf"
+sudo cp ~/dotfiles/scripts/.config/scripts/files/smb.conf /etc/samba/smb.conf
+echo "Arquivo copiado."
